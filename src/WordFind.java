7f6770c69851d451ceecb1f3f4344ca97bb4aab2
BT_1E10_33 <sample@bths.edu>
2022-03-14 10:56:10 -0400

    Initial commit

---
+++ src/WordFind.java
@@ -0,0 +1,8 @@
+public class WordFind
+{
+    public static void main(String[] args)
+    {
+        WordFindRunner wordle = new WordFindRunner();
+        wordle.run();
+    }
+}
---
+++ src/WordFindRunner.java
@@ -0,0 +1,18 @@
+public class WordFindRunner
+{
+    public void run()
+    {
+        public final String GREEN = ""
+        System.out.println("How To Play:" +
+                            "\nGuess the WORDLE in six tries" +
+                            "\nEach guess must be a valid five-letter word." +
+                            "\nHit the enter button to submit." +
+                            "\nAfter each guess, the color of the tiles will change to show how close your guess was to the word." +
+                            "\n--------------------------------------------------------------------------------------------------" +
+                            "\nExamples:" +
+                            "\n");
+
+
+
+    }
+}
